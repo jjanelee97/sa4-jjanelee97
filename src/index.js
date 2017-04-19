3dea0944c0ec3f0b6e519a48b10a9208b9ed60e9
Jane Lee <jane.lee.19@dartmouth.edu>
2017-04-18 21:18:13 -0400

    pushing starter pack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-let seconds = 0;
-function countSeconds() {
-  console.log(seconds);
-  $('#main').html(`You've been on this page for ${seconds} seconds`);
-  seconds += 1;
-}
-setInterval(countSeconds, 1000);
+const App = () => {
+  return <div className="test">All the React are belong to us!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('main'));
+//
+// let seconds = 0;
+// function countSeconds() {
+//   console.log(seconds);
+//   $('#main').html(`You've been on this page for ${seconds} seconds`);
+//   seconds += 1;
+// }
+// setInterval(countSeconds, 1000);
